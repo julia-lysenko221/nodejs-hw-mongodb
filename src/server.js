b9cfab283fac6ebe417e1dd2b65681c1434ac33b
juliya.lysenko <juliya.deleh@gmail.com>
2025-10-24 13:12:25 +0300

    commit2

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +17,9 @@ export const setupServer = () => {
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
   app.get('/', (req, res) => {
